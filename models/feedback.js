e2addf0952b8ca69fd8fc6a21c1e050838188e68
silkysmoothstone <48249178+silkysmoothstone@users.noreply.github.com>
2019-04-21 15:39:18 -0400

    just commenting our database models

--- models/feedback.js
+++ models/feedback.js
@@ -1,14 +1,14 @@
 var mongoose = require("mongoose");
 
 var feedbackSchema = new mongoose.Schema({
-    text: String,
+    text: String, //the text of the feedback
     author: {
         id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
+            type: mongoose.Schema.Types.ObjectId, //object id for feedback
+            ref: "User" //references user who left feedback
         },
-        username: String
+        username: String 
     }
 });
 
-module.exports = mongoose.model("Feedback", feedbackSchema);
+module.exports = mongoose.model("Feedback", feedbackSchema); //export it
