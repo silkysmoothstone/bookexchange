933faef8e5820168ae0d5fdd8e4981626be0c3cf
silkysmoothstone <48249178+silkysmoothstone@users.noreply.github.com>
2019-04-21 13:08:09 -0400

    Update book.js
    just some additional comments to the database model

--- models/book.js
+++ models/book.js
@@ -1,23 +1,23 @@
 var mongoose = require("mongoose");
 
 var bookSchema = new mongoose.Schema({
-    name: String,
-    price: String,
-    image: String,
-    imageId: String,
-    description: String,
-    isbn: String,
+    name: String, //name of the book
+    price: String, //the price listed
+    image: String, //image of the book
+    imageId: String, //image id
+    description: String, //description
+    isbn: String, //isbn
     sold: {
-        type: Boolean,
+        type: Boolean, //boolean if sold
         default: false
     },
     author: {
         id: {
-            type: mongoose.Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId, //object id referencing user
             ref: "User"
         },
-        username: String
+        username: String //username in our case the user's email
     }
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model("Book", bookSchema); //exports the model
