cfa96ffedcbba434652432c5ef6c554b7fea293b
silkysmoothstone <48249178+silkysmoothstone@users.noreply.github.com>
2019-04-21 22:10:20 -0400

    email fix and addition
    added sending email to buyer and fixed email with seller moving into the scope as well as changing how error handling with seller email worked

--- routes/transactions.js
+++ routes/transactions.js
@@ -49,6 +49,20 @@ router.post("/:id/buy", middleWare.isLoggedIn, function (req, res) {
                }
                buyer.transactions.push(transaction);
                buyer.save();
+               //email seller of transaction
+               const mailOptions = {
+               from: 'calubookex@gmail.com', // just a email address to use
+               to: 'buyer.username', // reciever name may need changed idk if this will work and can't test it without you
+               subject: 'DONOTREPLY Your book has sold', // subject to seller
+                 //confirmation messages
+               transporter.sendMail(mailOptions, function (err) {
+               if(err)
+                req.flash("Error", "Problem sending email!");
+                res.redirect("/books");
+               else
+                req.flash("success", "Thank you for your purchase. Check your email!");
+                res.redirect("/books");
+                });
            });
            //and seller transactions
            User.findById(foundBook.author.id, function (err, seller) {
@@ -58,25 +72,24 @@ router.post("/:id/buy", middleWare.isLoggedIn, function (req, res) {
               }
               seller.transactions.push(transaction); //pushes seller
               seller.save(); //saves it
+              //email seller of transaction
+               const mailOptions = {
+               from: 'calubookex@gmail.com', // just a email address to use
+               to: 'seller.username', // reciever name may need changed idk if this will work and can't test it without you
+               subject: 'DONOTREPLY Your book has sold', // subject to seller
+                 //confirmation messages
+               transporter.sendMail(mailOptions, function (err, info) {
+               if(err)
+                console.log(err)
+               else
+                console.log('Email Sent: ' + info.response)
+                });
+              };   
            });
        }
     });
-    //email seller of transaction
-    const mailOptions = {
-  from: 'calubookex@gmail.com', // just a email address to use
-  to: 'seller.username', // reciever name may need changed idk if this will work and can't test it without you
-  subject: 'DONOTREPLY Your book has sold', // subject to seller
-  html: '<p>Your html here</p>'// idk if we really need anything here if not marked for deletion
-    };   
-    //confirmation messages
-    transporter.sendMail(mailOptions, function (err) {
-   if(err)
-     req.flash("Error", "Problem sending email!");
-     res.redirect("/books");
-   else
-     req.flash("success", "Thank you for your purchase. Check your email!");
-    res.redirect("/books");
-    });
+  
+  
     
 });
 
